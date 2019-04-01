caf16df6eaf7adc7da4a2417f3e88a59935f3686
redbearin <perrigoj@bellsouth.net>
2019-04-01 12:28:54 -0700

    proof of life!!

--- server.js
+++ server.js
@@ -30,4 +30,27 @@ app.use(methodOverride(function(req, res){
   }
 }))
 
+//turn on server to listen on PORT
+app.listen(PORT, () => console.log(`Beerly here on PORT ${PORT}`));
 
+//server routes
+app.get('/', helloWorld);
+
+//generic route for all incorrect access
+app.use('*', (req, res) => errorHandler({status:404}, 'You have reached a page that does not exist.', res));
+
+//route callback functions
+function helloWorld(req, res) {
+  res.send('Hello World');
+}
+
+//helper functions
+function errorHandler(error, message, res) {
+  console.error(error);
+  if(message) {
+    console.log('Error message:', message);
+    if(res){
+      res.send(message);
+    }
+  }
+}
