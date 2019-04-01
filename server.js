0a0712df0ff72f80c832d6b90e26d08c103aca30
redbearin <perrigoj@bellsouth.net>
2019-04-01 11:38:17 -0700

    basic server configs

---
+++ server.js
@@ -0,0 +1,13 @@
+'use strict';
+
+//server requirements
+const express = require('express');
+const superagent = require('superagent');
+const methodOverride = require('method-override');
+const pg = require('pg');
+require('dotenv').config();
+
+//config of server (variables)
+const PORT = process.env.PORT;
+
+const client = new pg.Client(process.env.DATABASE_URL);
