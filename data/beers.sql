138f0c3a54e49f9f7ff8a063072de6932c368212
redbearin <perrigoj@bellsouth.net>
2019-04-02 11:29:05 -0700

    work work on API and DB

--- data/beers.sql
+++ data/beers.sql
@@ -1,22 +1,33 @@
 DROP TABLE IF EXISTS beers; 
-DROP TABLE IF EXISTS brewery;
-DROP TABLE IF EXISTS styles; 
+DROP TABLE IF EXISTS breweries;
+DROP TABLE IF EXISTS styles;
+DROP TABLE IF EXISTS ratings; 
 
-CREATE TABLE IF NOT EXISTS brewery(
-  id SERIAL PRIMARY KEY, 
+CREATE TABLE IF NOT EXISTS breweries(
+  id VARCHAR(32) PRIMARY KEY,  -- pulled from API data
+  brewery VARCHAR(255), -- name of brewery
+  website VARCHAR(255),
   time_stamp TIMESTAMP,
-  brewery VARCHAR(255), 
   location VARCHAR(255), 
-  socialmedia VARCHAR(255)
+  image VARCHAR(511)
 );
 
 CREATE TABLE IF NOT EXISTS styles(
-  id SERIAL PRIMARY KEY, 
+  id INT PRIMARY KEY,  -- pulled from API data
   time_stamp TIMESTAMP,
   name VARCHAR(255), 
   description TEXT,
+  abvmin DEC(5,3),
+  abvmax DEC(5,3),
   ibumin INT, 
   ibumax INT
+);
+
+CREATE TABLE IF NOT EXISTS ratings(
+  id SERIAL PRIMARY KEY, 
+  usernote TEXT,
+  userrating INT,
+  numvotes INT,
 );
 
 CREATE TABLE IF NOT EXISTS beers(
@@ -24,14 +35,14 @@ CREATE TABLE IF NOT EXISTS beers(
   name VARCHAR(255),
   beer_id VARCHAR(100),
   abv DEC(5,3),
-  style_name VARCHAR(255),
-  style_id INT, 
+  ibu INT,
   time_stamp TIMESTAMP, 
-  usernote TEXT,
-  userrate INT,
+  rating_id INT,
+  brewery_id VARCHAR(32),
+  style_id INT,
   FOREIGN KEY(style_id) REFERENCES styles(id),
-  brewery_id INT,
-  FOREIGN KEY(brewery_id) REFERENCES brewery(id)
+  FOREIGN KEY(brewery_id) REFERENCES breweries(id),
+  FOREIGN KEY(rating_id) REFERENCES ratings(id) 
 );
 
 
