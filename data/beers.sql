43deb31b8221d1ecbd6ca7179e876fabb375332f
redbearin <perrigoj@bellsouth.net>
2019-04-02 10:02:46 -0700

    working on server connect to db

--- data/beers.sql
+++ data/beers.sql
@@ -2,21 +2,6 @@ DROP TABLE IF EXISTS beers;
 DROP TABLE IF EXISTS brewery;
 DROP TABLE IF EXISTS styles; 
 
-CREATE TABLE IF NOT EXISTS beers(
-  id SERIAL PRIMARY KEY, 
-  name VARCHAR(255),
-  beer_id VARCHAR(100),
-  abv DEC(5,3),
-  style_name VARCHAR(255),
-  style_id INT, 
-  time_stamp TIMESTAMP, 
-  usernote TEXT,
-  userrate INT,
-  FOREIGN KEY(style_id) REFRENCES styles(id),
-  brewery_id INT,
-  FOREIGN KEY(brewery_id) REFRENCES brewery(id)
-);
-
 CREATE TABLE IF NOT EXISTS brewery(
   id SERIAL PRIMARY KEY, 
   time_stamp TIMESTAMP,
@@ -33,3 +18,21 @@ CREATE TABLE IF NOT EXISTS styles(
   ibumin INT, 
   ibumax INT
 );
+
+CREATE TABLE IF NOT EXISTS beers(
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255),
+  beer_id VARCHAR(100),
+  abv DEC(5,3),
+  style_name VARCHAR(255),
+  style_id INT, 
+  time_stamp TIMESTAMP, 
+  usernote TEXT,
+  userrate INT,
+  FOREIGN KEY(style_id) REFERENCES styles(id),
+  brewery_id INT,
+  FOREIGN KEY(brewery_id) REFERENCES brewery(id)
+);
+
+
+
