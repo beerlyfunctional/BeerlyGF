cae19005806c07eba3eca929d3b168bfa49fa7a9
herndont <herndontrey@gmail.com>
2019-04-02 17:10:34 -0700

    continued work on cookies

--- public/js/popUp.js
+++ public/js/popUp.js
@@ -1,5 +1,5 @@
 'use strict';
-//following function has been amended from an example initially found on W3 schools. https://www.w3schools.com/js/js_cookies.asp
+// following function has been amended from an example initially found on W3 schools. https://www.w3schools.com/js/js_cookies.asp
 function getCookie(agedCookies) {
   let name = agedCookies + '=';
   let decodedCookie = decodeURIComponent(document.cookie);
@@ -18,7 +18,7 @@ function getCookie(agedCookies) {
 
 function checkCookie() {
   let value = getCookie(agedCookies);
-  if (agedCookies === '21Years') {
+  if (value === '21Years') {
     $('#agePop').attr('style', 'display: none');
   }
 }
